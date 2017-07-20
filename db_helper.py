fce32f3342b65f935bc8202b309d0af1f066d5ef
oliver <o-lohr@gmx.de>
2017-07-20 14:25:42 +0200

    Save some write actions on the database

--- db_helper.py
+++ db_helper.py
@@ -4,6 +4,8 @@ import threading
 #Connection objects can't be used within mutiple threads, so a connection for every thread is needed
 #tid=thread-id
 db_connections={}
+#last write (thread-id)  to reduce commits on the database
+db_last_write=-1
 def get_con_for_id(tid):
     if tid in db_connections:
         return db_connections[tid]
@@ -13,14 +15,27 @@ def get_con_for_id(tid):
         return con
 
 def execute(*args):
-    db=get_con_for_id(threading.get_ident())
+    tid = threading.get_ident()
+    db=get_con_for_id(tid)
     db.execute(*args)
-    db.commit()
+    _commit(tid)
 
 def fetchall(*args):
     db=get_con_for_id(threading.get_ident())
     return db.execute(*args).fetchall()
 
+def _commit(tid):
+    """Check if commit is needed"""
+    global db_last_write
+    if db_last_write == -1:
+        db_last_write = tid
+    elif db_last_write != tid:
+        db_connections[db_last_write].commit()
+        db_last_write=tid
+    
+
 def close():
+    if db_last_write != -1:
+        db_connections[db_last_write].commit()
     for tid,con in db_connections.items():
         con.close()
