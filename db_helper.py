d7e16248a32697ff3751c6c8a22c5fc9e2979672
oliver <o-lohr@gmx.de>
2017-07-22 23:59:23 +0200

    Fixed multithread sqlite problem (hopefully...)

--- db_helper.py
+++ db_helper.py
@@ -1,41 +1,30 @@
 import sqlite3
 import threading
+import os
 
-#Connection objects can't be used within mutiple threads, so a connection for every thread is needed
-#tid=thread-id
-db_connections={}
-#last write (thread-id)  to reduce commits on the database
-db_last_write=-1
-def get_con_for_id(tid):
-    if tid in db_connections:
-        return db_connections[tid]
-    else:
-        con=sqlite3.connect('bot.db')
-        db_connections[tid]=con
-        return con
+#Fix multiple thread database access
+db_lock=threading.Lock()
+debug=bool(os.getenv('DEBUG'))
+
+db=sqlite3.connect('bot.db', check_same_thread=False)
 
 def execute(*args):
-    tid = threading.get_ident()
-    db=get_con_for_id(tid)
-    db.execute(*args)
-    _commit(tid)
+    with db_lock:
+        if debug:
+            print('executing',args)
+        db.execute(*args)
 
 def fetchall(*args):
-    db=get_con_for_id(threading.get_ident())
-    return db.execute(*args).fetchall()
+    with db_lock:
+        if debug:
+            print('fetching',args)
+        return db.execute(*args).fetchall()
 
-def _commit(tid):
-    """Check if commit is needed"""
-    global db_last_write
-    if db_last_write == -1:
-        db_last_write = tid
-    elif db_last_write != tid:
-        db_connections[db_last_write].commit()
-        db_last_write=tid
-    
+def commit():
+        with db_lock:
+            db.commit()
 
 def close():
-    if db_last_write != -1:
-        db_connections[db_last_write].commit()
-    for tid,con in db_connections.items():
-        con.close()
+    with db_lock:
+        db.commit()
+        db.close()
